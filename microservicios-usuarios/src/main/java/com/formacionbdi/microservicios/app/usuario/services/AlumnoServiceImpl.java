4624d5922a180f5c4c063c0cbaacdebb8c3f536c
Ruben-developer <rcontreras.dev@protonmail.com>
2021-08-06 03:09:09 -0400

    Buscar Alumnos por nombre o apellido usando operador like

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuario/services/AlumnoServiceImpl.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuario/services/AlumnoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.formacionbdi.microservicios.app.usuario.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.formacionbdi.microservicios.app.usuario.models.repository.AlumnoRepository;
 import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
@@ -9,6 +12,11 @@ import com.formaciondbi.microservicios.commons.services.CommonServiceImpl;
 @Service
 public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
 	
+	@Override
+	@Transactional(readOnly = true)
+	public List<Alumno> findByNombreOrApellido(String term){
+		return repository.findByNombreOrApellido(term);
+	}
 	
 
 }
